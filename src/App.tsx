b709716ca86afc0cd8a83697da9883b4d4117d7e
Sandrico Provo <Sprovo@outlook.com>
2022-08-26 08:55:32 -0300

    [feature] Adds apollo client.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
 
+import client from './Apollo/client';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
 import Layout from './components/Layout';
@@ -25,37 +27,39 @@ function App() {
     return (
         <>
             <GlobalStyles />
-            <BrowserRouter>
-                <Layout>
-                    <Header>
-                        <div>Bougette</div>
-                        <Navigation />
-                    </Header>
-                    <Routes>
-                        <Route path={ROUTES.HOME} element={<Home />} />
-                        <Route path={ROUTES.LOGIN} element={<Auth />} />
-                        <Route path={ROUTES.SIGNUP} element={<Auth />} />
-                        <Route
-                            path={ROUTES.STATEMENTS}
-                            element={
-                                <PrivateRoute>
-                                    <Statements />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route
-                            path={`${ROUTES.STATEMENTS}/:statementId`}
-                            element={
-                                <PrivateRoute>
-                                    <Statement />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route path="*" element={<Page404 />} />
-                    </Routes>
-                    <Footer>Hello Footer</Footer>
-                </Layout>
-            </BrowserRouter>
+            <ApolloProvider client={client}>
+                <BrowserRouter>
+                    <Layout>
+                        <Header>
+                            <div>Bougette</div>
+                            <Navigation />
+                        </Header>
+                        <Routes>
+                            <Route path={ROUTES.HOME} element={<Home />} />
+                            <Route path={ROUTES.LOGIN} element={<Auth />} />
+                            <Route path={ROUTES.SIGNUP} element={<Auth />} />
+                            <Route
+                                path={ROUTES.STATEMENTS}
+                                element={
+                                    <PrivateRoute>
+                                        <Statements />
+                                    </PrivateRoute>
+                                }
+                            />
+                            <Route
+                                path={`${ROUTES.STATEMENTS}/:statementId`}
+                                element={
+                                    <PrivateRoute>
+                                        <Statement />
+                                    </PrivateRoute>
+                                }
+                            />
+                            <Route path="*" element={<Page404 />} />
+                        </Routes>
+                        <Footer>Hello Footer</Footer>
+                    </Layout>
+                </BrowserRouter>
+            </ApolloProvider>
         </>
     );
 }
