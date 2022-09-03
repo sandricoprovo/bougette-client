42bbdb65d6e0ba48e3e550d7478bb46186d650a8
Sandrico Provo <Sprovo@outlook.com>
2022-09-03 16:19:18 -0300

    [feature] Adds getStatement query functionality.

--- src/components/Statements/StatementHeader.tsx
+++ src/components/Statements/StatementHeader.tsx
@@ -1,12 +1,20 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
+    border-bottom: 1px solid black;
+    padding: 2rem 0;
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
-    gap: 0.5rem;
+    gap: 1rem;
 
-    & > div {
+    & > div:nth-of-type(1) {
+        display: flex;
+        justify-content: space-between;
+        align-items: baseline;
+    }
+
+    & > div:nth-of-type(2) {
         display: flex;
         justify-content: space-between;
         align-items: center;
@@ -18,6 +26,7 @@ interface StatementHeaderProps {
     balance: number;
     totalIncome: number;
     totalExpenses: number;
+    createdOn: string;
 }
 
 export default function StatementHeader({
@@ -25,10 +34,14 @@ export default function StatementHeader({
     balance = 0,
     totalExpenses = 0,
     totalIncome = 0,
+    createdOn,
 }: StatementHeaderProps) {
     return (
         <Container>
-            <h1>{name}</h1>
+            <div>
+                <h1>{name}</h1>
+                <p>Created On: {createdOn}</p>
+            </div>
             <p>Balance: {balance}</p>
             <div>
                 <p>Total Income: {totalIncome} </p>
--- src/routes/Statement.tsx
+++ src/routes/Statement.tsx
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
-// import { useParams } from 'react-router';
+import { useParams } from 'react-router';
+import { useQuery } from '@apollo/client';
 
-import { mockStatement } from '../mock/mockStatement';
 import StatementHeader from '../components/Statements/StatementHeader';
 import LineItem from '../components/Statements/LineItem';
 import LineItemList from '../components/Statements/LineItemList';
+import { GET_STATEMENT } from '../apollo/queries';
+import { Statement as IStatement } from '../types/Statement';
 
 const Container = styled.div`
     display: flex;
@@ -14,62 +16,77 @@ const Container = styled.div`
 `;
 
 export default function Statement() {
-    // TODO: Make graphql call to fetch statement
-    // const { statementId } = useParams();
+    // Grabs statementId from url via react router.
+    const { statementId } = useParams();
+    const {
+        loading,
+        data: { getStatement: statement } = { getStatement: {} as IStatement },
+        error,
+    } = useQuery<{ getStatement: IStatement }>(GET_STATEMENT, {
+        variables: { statementId },
+    });
+
+    if (loading) return <h1>Loading...</h1>;
+    if (error) return <h1>Error...</h1>;
+
+    const { createdOn, expenses, incomes, label: statementLabel } = statement;
 
     return (
         <Container>
             <StatementHeader
-                name="Statement Name"
+                name={statementLabel}
+                createdOn={createdOn}
                 balance={0}
                 totalExpenses={0}
                 totalIncome={0}
             />
             <LineItemList title="Money In">
-                {mockStatement.incomes.map((income) => {
-                    const {
-                        label,
-                        amount,
-                        type,
-                        isRecurring,
-                        depositDate,
-                        id,
-                    } = income;
+                {incomes &&
+                    incomes.map((income) => {
+                        const {
+                            label,
+                            amount,
+                            type,
+                            isRecurring,
+                            depositDate,
+                            id,
+                        } = income;
 
-                    return (
-                        <LineItem
-                            key={id}
-                            label={label}
-                            amount={amount}
-                            type={type}
-                            isRecurring={isRecurring}
-                            depositDate={depositDate}
-                        />
-                    );
-                })}
+                        return (
+                            <LineItem
+                                key={id}
+                                label={label}
+                                amount={amount}
+                                type={type}
+                                isRecurring={isRecurring}
+                                depositDate={depositDate}
+                            />
+                        );
+                    })}
             </LineItemList>
             <LineItemList title="Money Out">
-                {mockStatement.expenses.map((expense) => {
-                    const {
-                        label,
-                        amount,
-                        type,
-                        isRecurring,
-                        withdrawDate,
-                        id,
-                    } = expense;
+                {expenses &&
+                    expenses.map((expense) => {
+                        const {
+                            label,
+                            amount,
+                            type,
+                            isRecurring,
+                            withdrawDate,
+                            id,
+                        } = expense;
 
-                    return (
-                        <LineItem
-                            key={id}
-                            label={label}
-                            amount={amount}
-                            type={type}
-                            isRecurring={isRecurring}
-                            withdrawDate={withdrawDate}
-                        />
-                    );
-                })}
+                        return (
+                            <LineItem
+                                key={id}
+                                label={label}
+                                amount={amount}
+                                type={type}
+                                isRecurring={isRecurring}
+                                withdrawDate={withdrawDate}
+                            />
+                        );
+                    })}
             </LineItemList>
         </Container>
     );
